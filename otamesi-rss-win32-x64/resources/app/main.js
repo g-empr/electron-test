fd6a155573887cb86236ea1875b84be92be8bc13
yoshida kohei <mc00102@members.co.jp>
2017-05-08 11:27:51 +0900

    hotfix

--- otamesi-rss-win32-x64/resources/app/main.js
+++ otamesi-rss-win32-x64/resources/app/main.js
@@ -1,10 +1,14 @@
-const electron = require('electron');
-const {app} = electron;
-const {BrowserWindow} = electron;
+
+const {app, BrowserWindow} = require('electron');
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600, webPreferences: { nodeIntegration: false } });
+  win = new BrowserWindow({
+    width: 400,
+    height: 900,
+    webPreferences: { nodeIntegration: false },
+    resizable : false
+    });
   win.loadURL(`file://${__dirname}/index.html`);
   // win.webContents.openDevTools(); 開発ツールは非表示に変更
   win.on('closed', () => {
